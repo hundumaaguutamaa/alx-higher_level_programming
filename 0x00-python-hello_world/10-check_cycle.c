8e10ae054440237a26c76c42d4089a5146e6a48f
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-08 02:34:50 +0000

    lists.h

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: a singly-linked list.
+ *
+ * Return: 0 If there is no cycle. 
+ *        1 If there is a cycle. 
+ */
+int check_cycle(listint_t *list)
+{
+
+}
+

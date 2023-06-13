af6779bbef5857eae066377d3affa2e1dc97511c
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-13 15:51:59 +0000

    2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+	list2 = len(my_list):
+	if idx is <0 or idx >= list2:
+		return my_list
+	else:
+		my_list[idx] = element
+			return my_list
+

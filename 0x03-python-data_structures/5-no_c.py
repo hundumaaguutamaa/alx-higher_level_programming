763ad6c899c57ea9a73b617c4c9e83c455110e0e
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-13 19:34:26 +0000

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+new_string = ""
+index = 0
+while index < len(my_string):
+    if my_string[index] != 'c' and my_string[index] != 'C':
+        new_string += my_string[index]
+    index += 1
+return new_string
+

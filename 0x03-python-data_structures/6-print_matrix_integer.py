493b6da48dcd85a6f2f644cba950f767d6259b90
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-13 19:53:38 +0000

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for num in matrix:
+        num_count = len(num)
+        for item in range(num_count):
+            print(" {} ".format(num[item]), end="")
+            if item < num_count - 1:
+                print(" ", end="")
+        print("")
+

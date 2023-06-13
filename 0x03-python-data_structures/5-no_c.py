aaa5088c018b661569845a023720f770b1e5dc89
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-13 19:49:08 +0000

    6-main.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,5 +6,5 @@ while index < len(my_string):
     if my_string[index] != 'c' and my_string[index] != 'C':
         new_string += my_string[index]
     index += 1
-return new_string
+	return new_string
 

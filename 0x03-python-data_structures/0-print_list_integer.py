bd42a49d95732f9de15824df45c7e63ff993cd49
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-13 14:53:46 +0000

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,6 @@
 print_list_integer = __import__('0-print_list_integer').print_list_integer
 
 my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
-print("my_list".format(my_list)
+print_list_integer(my)
+print(" {} ".format(my_list))
 

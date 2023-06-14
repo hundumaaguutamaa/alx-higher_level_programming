1cb3c0a22bf91727301a045ebdead6a2e3b2632d
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-14 21:48:32 +0000

    4-main.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+   #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    comm_elemenet = []
+    el_result = []
+
     for letter in set_1:
         if letter in set_2:
-            result[len(letter):] = [letter]
-            
-    return result
+           el_result.append(letter)
+    return el_result
 

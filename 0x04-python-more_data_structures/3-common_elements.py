6488bd579647040c721c8009232497d2c2ff7689
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-14 21:31:11 +0000

    3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    comm_elemenet = []
+    for letter in set_1:
+        if letter in set_2:
+            result[len(letter):] = [letter]
+            
+    return result
+

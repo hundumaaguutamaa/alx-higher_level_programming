1fa7baccbbcf54aeda4bdb909d13df0bcf4fe5f3
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-16 08:18:54 +0000

    100-main.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-multiply_list_map = __import__('11-multiply_list_map').multiply_list_map
-
+def multiply_list_map(my_list=[], number=0):
+    return new_list(map(lambda x: x * number, my_list))

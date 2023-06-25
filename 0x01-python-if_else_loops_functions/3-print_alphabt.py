42d0318c76473fd26a4d4a330280597cdfbe9b9d
hundumaaguutamaa <hundumaa.guutamaa>
2023-06-25 02:13:17 -0700

    3-a

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-        if i != 101 and i != 113:
-            print("{:s}".format(chr(i)), end='')
-
+for c in range(97, 123):
+        if c != 101 and c != 113:
+                print("{:c}".format(c), end='')

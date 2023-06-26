c9d6cb4ff161d11b0ed3800ccb45bd633cad4933
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-26 09:12:49 +0000

    print element of list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-
+def safe_print_list(my_list=[], x=0):
+    count_inlist = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        except:
+            print("Not in the list")
+        else:
+            count_inlist += 1
+    print()
+    return (count_inlist)

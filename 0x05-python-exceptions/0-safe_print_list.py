371ada6d9e997582db7ba7cf83fee8348c9d37ff
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-27 01:43:03 +0000

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,17 @@
+#!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
     count_inlist = 0
+
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-            count_inlist += 1
-        except IndexError:
+        except:
             break
+        else:
+            count_inlist += 1
+
     print()
     return count_inlist
 

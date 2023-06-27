ae7316a9eb9ec1cdef54b36b482fe91e997d11cb
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-06-27 01:35:45 +0000

    loop order

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count_inlist = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-        except:
-            break
-        else:
             count_inlist += 1
+        except IndexError:
+            break
     print()
-return(count_inlist)
+    return count_inlist
+

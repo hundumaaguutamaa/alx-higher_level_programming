170d8bfc2eafcc4fb09f55bb27c0b6d4e0e466ac
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-06-28 23:50:52 +0300

    Line spaces

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -5,8 +5,7 @@
 
 class Square:
     """Representing a square."""
-
-
+    
     def __init__(self, size=0):
         """Initializes a new square.
 
@@ -15,12 +14,10 @@ class Square:
         """
         self.size = size
 
-
     @property
     def size(self):
         """Get,set current size of square."""
         return (self.__size)
-
 
     @size.setter
     def size(self, value):
@@ -30,11 +27,9 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-
     def area(self):
         """Return area of the square, current area."""
         return (self.__size * self.__size)
-
 
     def __eq__(self, other):
         """Define the equals to comparision to a Square."""
@@ -44,21 +39,17 @@ class Square:
         """Define the not equal comparison to a Square."""
         return self.area() != other.area()
 
-
     def __lt__(self, other):
         """Define lessthan comparison to a Square."""
         return self.area() < other.area()
-
 
     def __le__(self, other):
         """Define the lessthan or equal comparison to a Square."""
         return self.area() <= other.area()
 
-
     def __gt__(self, other):
         """Define the greaterthan comparison to a Square."""
         return self.area() > other.area()
-
 
     def __ge__(self, other):
         """Define the greaterthan or equal compmarison to a Square."""

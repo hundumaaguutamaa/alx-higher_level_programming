09743e824ca81619032f1609c16efed9a407911e
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-06-28 23:35:46 +0300

    Indentation of documentation style

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Define python MagicClass to do as the same bytecode provided""" 
+"""Define python MagicClass to do as the same bytecode provided"""
 
 import math
 
@@ -11,7 +11,7 @@ class MagicClass:
     def __init__(self, radius=0):
         """Initialize a MagicClass with radius.
 
-	Arg:
+    Arg:
             radius (float or int):radius of new MagicClass.
         """
         self.__radius = 0
@@ -22,7 +22,3 @@ class MagicClass:
     def area(self):
         """Return area of the MagicClass."""
         return (self.__radius ** 2 * math.pi)
-
-    def circumference(self):
-        """Returns circumference of the MagicClass."""
-        return (2 * math.pi * self.__radius)

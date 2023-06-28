ae16cad002a893930efa3c06e33a164084cdecce
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-06-28 23:46:39 +0300

    New line at end of code

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -22,3 +22,8 @@ class MagicClass:
     def area(self):
         """Return area of the MagicClass."""
         return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Returns circumference of the MagicClass."""
+        return 2 * math.pi * self.__radius
+        

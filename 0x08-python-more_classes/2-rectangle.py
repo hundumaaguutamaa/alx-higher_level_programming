36a109ec9b57c14ffb588debc1a79d05e7d46707
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-07-05 20:33:08 +0000

    Added pydoc

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,17 +1,21 @@
+#!/usr/bin/python3
+
+
 class Rectangle:
-   
+    """Defined a class Rectangle"""
 
     def __init__(self, width=0, height=0):
-       
+       """Initializes a rectangle method with attributes"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-     
+     """Returns a width method"""
         return self.__width
 
     @width.setter
+
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -21,7 +25,7 @@ class Rectangle:
 
     @property
     def height(self):
-      
+      """Returns a height method"""
         return self.__height
 
     @height.setter
@@ -33,12 +37,12 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-     
+        """Returns a area method"""
         return (self.__width * self.__height)
 
     def perimeter(self):
-     
+        """Returns a perimeter method"""
         if self.__width == 0 or self.__height == 0:
             return (0)
-            
+            """Returns perimeter of rectangle"""
         return ((self.__width * 2) + (self.__height * 2))

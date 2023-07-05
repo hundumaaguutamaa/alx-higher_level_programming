92fa8742965a2c9a6498e760f64abe6e9e87950e
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-07-05 21:25:05 +0000

    3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+"""Create a class Rectangle """
+
+
+class Rectangle:
+    """A Rectangle class"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Retrieve width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set attribute"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set the attribute"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """Get area of the rectangle"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """Get perimeter of the rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.height + self.width)
+
+    def __str__(self):
+        """Sets the Rectangle object."""
+        rectangle = ""
+
+        if self.__width > 0 and self.__height > 0:
+            rectangle = '\n'.join(['#' * self.__width for _ in range(self.__height)])
+
+        return rectangle[:-1]

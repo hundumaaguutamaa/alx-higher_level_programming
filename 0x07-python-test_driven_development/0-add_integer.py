6de1bbe269f2984895f47cc54a895e49a4f1ac31
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-07-06 22:48:00 +0300

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -20,10 +20,10 @@ def add_integer(a, b=98):
     """
 
     if type(a) not in [int, float]:
-    raise TypeError("a must be an integer or a float")
+        raise TypeError("a must be an integer or a float")
 
     if type(b) not in [int, float]:
-    raise TypeError("b must be an integer or a float")
+        raise TypeError("b must be an integer or a float")
 
 
     a = int(a)

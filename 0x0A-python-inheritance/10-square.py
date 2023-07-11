9d92660e06579d6bb76c68f0e3cdf14365d41bb2
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-07-11 05:17:31 +0300

    Imported BaseGeometry and Rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+""" Import BaseGeometry and Rectangle """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 """Write a class square that inherites from rectangle"""
 
 

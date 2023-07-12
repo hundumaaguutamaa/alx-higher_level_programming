521b3a79c95a2b6772dd5fb3e4562d0bd129b7b4
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-07-12 02:26:30 +0000

    Import json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,9 +5,6 @@
 import json
     """Import JSON module"""
 
-
 def to_json_string(my_obj):
-    """
-     A function that returns the JSON representation of an object (string)
-    """
+    """ A function that returns the JSON representation of an object (string)."""
     return json.dumps(my_obj)

b17796b25638e8ecc33e5cf7ae1dc1c1b6df9d49
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-07-12 02:37:32 +0000

    Indentation error

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-	 """JSON representation"""
+"""JSON representation"""
 
 import json
-	"""Import json"""
-
+"""Import json"""
 def to_json_string(my_obj):
-	""" A function that returns the JSON representation of an object (string)."""
-	return json.dumps(my_obj)
+    """ A function that returns the JSON representation of an object (string)."""
+    return json.dumps(my_obj)

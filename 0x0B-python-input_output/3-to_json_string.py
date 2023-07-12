2009069f7d1e88929c4a4bf8f4e789d8a422fb37
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-07-12 02:31:15 +0000

    Indentation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,6 +5,7 @@
 import json
 	"""Import JSON module"""
 
+
 def to_json_string(my_obj):
 	""" A function that returns the JSON representation of an object (string)."""
-    return json.dumps(my_obj)
+	return json.dumps(my_obj)

2d94fb37d24292488df7b2cafaec6bec865e6989
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-07-13 02:03:09 +0000

    Documentation

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -15,6 +15,8 @@ def pascal_triangle(n):
         row = [1]
         for j in range(1, i):
             row.append(res[i-1][j-1] + res[i-1][j])
+
         row.append(1)
         res.append(row)
     return res
+

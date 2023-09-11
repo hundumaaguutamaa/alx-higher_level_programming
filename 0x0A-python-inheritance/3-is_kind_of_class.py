6b98ed7f85d9645e904ce75bdc9464ef9a9d8114
Hunduma <hundumaa.guutamaa@gmail.com>
2023-09-11 07:37:42 +0000

    object is instance of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""a class is_kind_of_class """
+def is_kind_of_class(obj, a_class):
+	""" True if object is instance of a class else false """
+
+    return isinstance(obj, a_class)

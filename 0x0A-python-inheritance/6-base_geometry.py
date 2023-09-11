bf51e085cf721e09f0d7e59c514bc1d4808587cb
Hunduma <hundumaa.guutamaa@gmail.com>
2023-09-11 12:58:03 +0000

    class, method, exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" class BaseGeometry """
+
+class BaseGeometry:
+    """Clsss that defines geometry"""
+    def area(self):
+        raise Exception("area() is not implemented")

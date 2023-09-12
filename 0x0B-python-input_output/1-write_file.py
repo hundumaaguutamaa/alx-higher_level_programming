e15c500942264b5f6456814450455edb163132f4
Hunduma <hundumaa.guutamaa@gmail.com>
2023-09-12 18:33:06 +0000

    write a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Define the function that write a file"""
+
+
+def write_file(filename="", text=""):
+	"""Write a string to a UTF-8 text file"""
+	with open(filename, "w", encoding=utf-8") as h:
+		return h.write(text)

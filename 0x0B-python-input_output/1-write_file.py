8b47767d0a79e0e40e55f3cfe096776f94acd81d
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-09-12 21:44:19 +0300

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,5 @@ def write_file(filename="", text=""):
 	"""Write a string to a UTF-8 text file"""
 	
 	with open(filename, "w", encoding="utf-8") as h:
+		""" writing the file with encoding utf-8"""
 		return h.write(text)

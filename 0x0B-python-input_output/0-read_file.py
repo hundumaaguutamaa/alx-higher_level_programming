de061737e65a2f0720e216a91f4a3d504d8f145c
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-09-12 21:22:46 +0300

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
 	"""Read fie and prints to stdout"""
 	with open(filename, encoding="utf-8") as h:
+		"""Opening the file with encoding utf-8"""
 		print(h.read(), end="")

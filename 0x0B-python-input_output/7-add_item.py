777c09143f11b7179f4f71ad33ed8c3932d7c590
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-09-13 05:41:17 +0300

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,6 +2,8 @@
 """"Add all arguments to a Python list"""
 
 import sys
+import os
+import json
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file

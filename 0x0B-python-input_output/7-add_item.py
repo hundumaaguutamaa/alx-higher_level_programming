9a41b9a81a9673e615614b3c073926381e2b3cb1
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-09-13 04:53:58 +0300

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
 	load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        all_items = load_from_json_file("add_item.json")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        all_items = []
-    all_items.extend(sys.argv[1:])
-    save_to_json_file(all_items, "add_item.json")
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")

e39c60798d6f246afdbdbc68d88209cdf1f4ac81
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-09-13 05:14:32 +0300

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,9 +8,9 @@ class Student:
 	def __init__(self, first_name, last_name, age):
         	"""Inistantiate a new Student"""
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+        	self.first_name = first_name
+        	self.last_name = last_name
+        	self.age = age
 
 	def to_json(self):
         	"""Retrive a dictionary representation of the Student instance"""

69e5bfac77cc6d4839ffb66d0bc4d27569463fb7
Hunduma <hundumaa.guutamaa@gmail.com>
2023-09-13 12:11:49 +0000

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""""Add all arguments to a Python list"""
+""""Adds all arguments to a Python list"""
 
 import sys
-import os
-import json
+import unicodedata 
 
 if __name__ == "__main__":
+    
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        '6-load_from_json_file').load_from_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     name = "add_item.json"
     arg = sys.argv[1:]
@@ -19,5 +18,5 @@ if __name__ == "__main__":
     except FileNotFoundError:
         h = []
 
-	h.extend(arg)
+    h.extend(arg)
     save_to_json_file(h, name)

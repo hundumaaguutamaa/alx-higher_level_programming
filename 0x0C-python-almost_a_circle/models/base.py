82bc57c75cf1cb9f0a46836e4c06639e011a1298
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-09-22 20:23:59 +0300

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,3 +14,10 @@ class Base:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Defining static method that returns json string representation."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)

1db51e043e342e2f60e6cf3a8b7a6b18cba8e5a0
Hunduma <hundumaa.guutamaa@gmail.com>
2023-09-22 20:28:08 +0000

    test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,1 +1,10 @@
-test_rectangle.py
+#!/usr/bin/python3
+
+"""unittests for models/rectangle.py."""
+
+import io
+import sys
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,1 +1,10 @@
-test_square.py
+#!/usr/bin/python3
+
+""" unittests for models/square.py."""
+
+import io
+import sys
+import unittest
+from models.base import Base
+from models.square import Square
+

a05f8542bb50af34c70022670cfcc15d9914a97e
Hunduma <hundumaa.guutamaa@gmail.com>
2023-09-22 20:22:06 +0000

    test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,1 +1,9 @@
-test_base.py
+#!/usr/bin/python3
+"""unittests for base.py."""
+
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+

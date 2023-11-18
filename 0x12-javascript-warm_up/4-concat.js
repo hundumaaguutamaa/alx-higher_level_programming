df4d672cc45635a91334a4de1fb3c8d70d96803a
root <hundumaa.guutamaa@gmail.com>
2023-11-18 11:45:35 +0000

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,4 +3,4 @@
 const process = require('process');
 const first_Arg = process.argv;
 const second_Arg = process.argv;
-console.log(first_Arg[1] + 'is' + second_Arg[2]);
+console.log(first_Arg[2] + 'is' + second_Arg[3]);

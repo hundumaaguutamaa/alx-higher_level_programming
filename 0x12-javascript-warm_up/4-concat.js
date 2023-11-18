9995800f7b90ee86dd5dcede28dd28764f759199
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-11-18 07:32:48 -0500

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 const process = require('process');
-const first_Arg = process.argv;
-const second_Arg = process.argv;
-console.log(first_Arg[2] + 'is' + second_Arg[3]);
+
+const aArg = process.argv;
+const bArg = process.argv;
+console.log(aArg[2] + ' is ' + bArg[3]);

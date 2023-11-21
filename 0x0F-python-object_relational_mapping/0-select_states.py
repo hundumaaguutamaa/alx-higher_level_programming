c1012028b904aafb99c6fbe821b96a242ec6cf1f
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-11-20 21:05:45 -0500

    db nme updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """Script list all states from database hbtn_0e_0_usa"""
 
-import MySQLdb
+import MYSQLdb
 from sys import argv
 
 if __name__ == '__main__':
 
     """   Access to databases and get states from databases. """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306
-                         password=argv[2], db=argv[3])
-    cur = dbname.cursor()
+    db = MYSQLdb.connect(host="localhost", user=argv[1], port=3306
+                         passwd=argv[2], db=argv[3])
+    
+    cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
 

5d13c0b744eb90af52409fc41634ed2b64f360ca
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-11-20 21:10:12 -0500

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == '__main__':
 
     """   Access to databases and get states from databases. """
-    db = MYSQLdb.connect(host="localhost", user=argv[1], port=3306
+    db = MYSQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
     
     cur = db.cursor()

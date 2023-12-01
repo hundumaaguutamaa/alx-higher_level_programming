8f09a916ce34f6f39172b54241c6b1c704045cb7
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-11-30 19:33:48 -0500

    inds a peak in a list of unsorted integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,20 +3,22 @@
 
 
 def find_peak(list_of_integers):
-     """Returns a peak in a list of unsorted integers."""
+     """ Returns a peak in a list of unsorted integers."""
     if not list_of_integers:
         return None
 
-    l, h = 0, len(list_of_integers) - 1
+    size = len(list_of_integers)
+    low, high = 0, size - 1
 
-    while l < h:
-        m = (l + h) // 2
+    while low <= high:
+        mid = (low + high) // 2
+        peak = list_of_integers[mid]
 
-        if list_of_integers[m] < list_of_integers[m + 1]:
-            
-            l = m + 1
-            
+        if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+            return peak
+        elif peak < list_of_integers[mid - 1]:
+            high = mid - 1
         else:
-            h = m
+            low = mid + 1
 
-    return list_of_integers[l]
+    return None

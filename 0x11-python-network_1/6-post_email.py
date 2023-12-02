fda1a9bbf4d2876649684612a712bb50afa08d65
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-12-02 04:13:12 +0300

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""sends a POST request to the passed URL with the email as a parameter"""
+"""Sends a POST request to the passed URL with the email as a parameter"""
 
 import sys
 import requests
 
-
 if __name__ == "__main__":
-    """ get the url from the command line """ 
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <URL> <email>")
+        sys.exit(1)
+
+    # Get the URL and email from the command line
     url = sys.argv[1]
     email = sys.argv[2]
 
-    """ create an email  dictionary"""
+    # Create an email dictionary
     e_data = {'email': email}
 
-    # send a POST request to the URL
+    # Send a POST request to the URL
     response = requests.post(url, data=e_data)
-    print(response.text)
 
+    # Print the response text
+    print(response.text)
+    

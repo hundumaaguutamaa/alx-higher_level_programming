2ec6f67c189b8bdd8f169a672a06dd8f2add4e3c
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-12-01 16:03:57 -0800

    script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
+
+import urllib
+from urllib import request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with request.urlopen(url) as response:
+        body = response.read()
+
+        print(f"Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body.decode('utf-8')}")
+

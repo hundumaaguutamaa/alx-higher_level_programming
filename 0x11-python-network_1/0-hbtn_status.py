4fcad3d236d554a0ca500f87e32a3ebfbbdd4c2b
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-12-02 03:12:52 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """ Python script that fetches https://alx-intranet.hbtn.io/status """
 
-import urllib
 from urllib import request
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
 
     with request.urlopen(url) as response:
-        body = response.read()
+        response_body = response.read()
 
         print(f"Body response:")
-        print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body.decode('utf-8')}")
-        print(f"\t- utf8 content: {body.format(response_body.decode('utf-8')))
+        print(f"\t- type: {type(response_body)}")
+        print(f"\t- content: {response_body.decode('utf-8')}")
+        print(f"\t- utf8 content: {response_body.decode('utf-8')}")

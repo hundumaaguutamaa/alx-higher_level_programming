41f138b61457172bf0ee06275a05800767baa439
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-12-01 17:37:42 -0800

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Uses the GitHub API to display the user id."""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Replace 'YOUR_ACCESS_TOKEN' with your actual personal access token
+    username = sys.argv[1]
+    access_token = sys.argv[2]
+
+    # URL for the authenticated user endpoint in GitHub API
+    url = "https://api.github.com/user"
+
+    # Set up the authentication using Basic Authentication with the access token
+    auth = (username, access_token)
+
+    # Send a GET request to the GitHub API with Basic Authentication
+    response = requests.get(url, auth=auth)
+
+    # Parse the JSON response
+    user_data = response.json()
+
+    # Display the user id
+    print(f"User ID: {user_data['id']}")
+
+    print(f"Error: {response.status_code}. Unable to fetch user data.")
+

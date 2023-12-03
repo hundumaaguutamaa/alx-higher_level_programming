de4d4257b2c78d8973a59c76af691ef5946f92e1
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-12-03 03:56:35 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
+
 const i = process.argv[2];
 
-function factorial (i) {
+function factorial(i) {
+  // Base cases: 0! and 1! are both 1
   if (i === 0 || i === 1 || i === undefined || isNaN(i)) {
     return 1;
-  } else { return (i * factorial(i - 1));
+  } else {
+    // Recursive case: i! = i * (i-1)!
+    return i * factorial(i - 1);
+  }
 }
+
 console.log(factorial(Number(i)));

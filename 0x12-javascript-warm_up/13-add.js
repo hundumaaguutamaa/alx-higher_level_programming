b426a132f6158b3b1d23d59877f005a2a7b69b04
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-12-03 04:22:39 +0300

    module for exporting

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
-const result = myModule.add(3, 5);
-console.log(result); 
+# an object, accessible to other modules that require or import the module. 
+exports.add = function (a, b) {
+  return a + b;
+};

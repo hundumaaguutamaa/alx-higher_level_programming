0c916f016047c6a7520e81bf15c49e17e8148a20
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-12-03 04:25:37 +0300

    Update 13-add.js

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-# an object, accessible to other modules that require or import the module. 
-exports.add = function (a, b) {
+
+function add (a, b) {
   return a + b;
-};
+}
+exports.add = add;

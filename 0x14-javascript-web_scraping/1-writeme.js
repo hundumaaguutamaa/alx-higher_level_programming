5a35049653b691e72e580c7585bcab936dc90029
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-01-08 01:49:10 -0800

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script that writes a string to a file. 
+
+const filesys = require('fs');
+try {
+    filesys.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, result) { if (err) console.log(err); });
+  } catch (err) {
+    console.log(err);
+  }
